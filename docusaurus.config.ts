59bf74af4717cf8e44bf0ff0479bf426b7ee7422
Max <max.pcgt@gmail.com>
2024-11-02 15:41:17 +0100

    👷🏻 build: Modification of the site configuration - v.1.0.6

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'saguenay', // Usually your GitHub org/user name.
   projectName: 'saguenay-help', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
